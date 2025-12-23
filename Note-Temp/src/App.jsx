40c749c979664242c1f6497a82df32ded3d14f49
Ayman Ahmed <ayman293.mon@gmail.com>
2025-12-23 23:11:21 +0200

    Setup json-server and implement data fetching

--- Note-Temp/src/App.jsx
+++ Note-Temp/src/App.jsx
@@ -1,10 +1,24 @@
-import { use, useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes || []);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
   const NoteToShow = showAll ? notes : notes.filter(note => note.important);
+  const hook = () => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+}
+
+useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
+  
   const addNote = (e) => {
     e.preventDefault();
     setNotes(notes.concat({ id: String(notes.length + 1), content: newNote, important: Math.random() < .5 }));
