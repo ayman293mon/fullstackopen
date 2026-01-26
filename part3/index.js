980fe84935788df5548c805ec053ca36afa481f0
Ayman Ahmed <ayman293.mon@gmail.com>
2026-01-26 16:01:40 +0200

    deploying full app

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.json());
 morgan.token('info', (req, res) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : '';
